4c49fc258999e88d0a132aa86a760d586d097cbb
Ryan Chang <ryan.changj@gmail.com>
2014-08-12 15:59:06 +0800

    Products

---
+++ app/controllers/api/products_controller.rb
@@ -0,0 +1,10 @@
+class Api::ProductsController < ApplicationController
+	def show
+		@product = Product.find(params[:id])
+		render json: @product
+	end
+
+	def index
+		render json: Product.all
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,10 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+
+  namespace :api do
+    resources :products
+  end  
 
   # Example resource route with options:
   #   resources :products do
